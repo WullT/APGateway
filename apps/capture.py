b9540c8de3e2ad13d6dd372ad74ebde781a56b5b
WullT <timeo.wullschleger@fhnw.ch>
2022-01-18 10:33:06 +0100

    update timeouts

--- apps/capture.py
+++ apps/capture.py
@@ -11,8 +11,8 @@ import shutil
 import sqliteadapter
 from dataclasses import dataclass
 
-check_interval = 10 # seconds to wait before checking again
-connect_timeout = 2
+check_interval = 5 # seconds to wait before checking again
+connect_timeout = 1
 read_timeout = check_interval-connect_timeout
 
 @dataclass
