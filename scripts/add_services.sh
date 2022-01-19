e5129629ea40f2be0abd22e97fe2faedc001e933
WullT <timeo.wullschleger@fhnw.ch>
2022-01-19 11:56:07 +0100

    add registerserver, remove discovery

--- scripts/add_services.sh
+++ scripts/add_services.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-sudo cp /home/pi/APGateway/services/discovery.service /etc/systemd/system/
+#sudo cp /home/pi/APGateway/services/discovery.service /etc/systemd/system/
 sudo cp /home/pi/APGateway/services/capture.service /etc/systemd/system/
 sudo cp /home/pi/APGateway/services/configserver.service /etc/systemd/system/
+sudo cp /home/pi/APGateway/services/registrationserver.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable discovery.service
+#sudo systemctl enable discovery.service
+sudo systemctl enable registrationserver.service
 sudo systemctl enable capture.service
 sudo systemctl enable configserver.service
 
-sudo systemctl start discovery.service
+#sudo systemctl start discovery.service
+sudo systemctl start registrationserver.service
 sudo systemctl start configserver.service
 sudo systemctl start capture.service
--- scripts/clone_install_add_services.sh
+++ scripts/clone_install_add_services.sh
@@ -9,15 +9,18 @@ pip3 install werkzeug
 cd /home/pi
 git clone https://github.com/WullT/APGateway.git
 
-sudo cp /home/pi/APGateway/services/discovery.service /etc/systemd/system/
+#sudo cp /home/pi/APGateway/services/discovery.service /etc/systemd/system/
 sudo cp /home/pi/APGateway/services/capture.service /etc/systemd/system/
 sudo cp /home/pi/APGateway/services/configserver.service /etc/systemd/system/
+sudo cp /home/pi/APGateway/services/registrationserver.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable discovery.service
+#sudo systemctl enable discovery.service
+sudo systemctl enable registrationserver.service
 sudo systemctl enable capture.service
 sudo systemctl enable configserver.service
 
-sudo systemctl start discovery.service
+#sudo systemctl start discovery.service
+sudo systemctl start registrationserver.service
 sudo systemctl start configserver.service
 sudo systemctl start capture.service
